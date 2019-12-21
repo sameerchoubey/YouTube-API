d651616b101dfbc3d2cf8b8f8467283530c39e49
Sameer Choubey <sameer.choubey98@gmail.com>
2019-12-21 18:37:27 +0530

    Work done till now. a) Initialized Django inside a virtual enviroment. b) Created an app named latest_videos that will contain our API. c) Created models and done migrations.

---
+++ latest_videos/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import main_db
+
+# Register your models here.
+admin.site.register(main_db)
---
+++ latest_videos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LatestVideosConfig(AppConfig):
+    name = 'latest_videos'
