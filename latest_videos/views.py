42ef13ba87a571677d3637e4297ae11d0d27f22e
Sameer Choubey <sameer.choubey98@gmail.com>
2019-12-22 14:41:37 +0530

    Adding support for multiple API keys, incase quota is exhausted.

--- latest_videos/views.py
+++ latest_videos/views.py
@@ -9,46 +9,63 @@ import threading
 import random
 
 api_key = 'AIzaSyCJtkbrA7_U3SjJYHEAU5Ha0tZzOdsSqy8'
+api_key_list = ['AIzaSyCJtkbrA7_U3SjJYHEAU5Ha0tZzOdsSqy8',
+                'AIzaSyCp-442KSJuHyJv8k0YwRYd6LG_oeCWkxg',
+                'AIzaSyCP_UQVrFtURgGwiQpSh7FXv68OfLVurjA',
+                'AIzaSyCP_UQVrFtURgGwiQpSh7FXv68OfLVurjA',
+                'AIzaSyAfwG4c9xak4ktEFSNAYK2fxQKLzMRGGJk',
+                'AIzaSyCQpXJGFRBA7uWmicOf2iy065QMXqjOccw',
+                'AIzaSyCvM6fyA3uTTyfXmoIOUStrdgG_QHobxOw',
+                'AIzaSyAbi4YyRr2gQkOAytW4HD07Mt8hR-ctxtw',
+                'AIzaSyCAOVUfWBln3bmWNNJEg4s00sOhxgsqCFU',
+                'AIzaSyDcySmOFch4tzoRmfQlcDS2nybJWoxETq0']
 keyword = 'Premier League'
 
 
 def fetch_from_youtube(keyword, api_key):
+    list_index = 0
     while True:
         youtube = build('youtube', 'v3', developerKey=api_key)
         req = youtube.search().list(q=keyword, part='snippet', type='video', maxResults=50)
-        res = req.execute()
 
-        for item in res['items']:
-            print(item['snippet']['title'])
-            fetched_video_id = item['id']['videoId']
-            found = True
-            try:
-                m = main_db.objects.get(video_id=fetched_video_id)
-            except ObjectDoesNotExist:
-                found = False
+        try:
+            res = req.execute()
+            for item in res['items']:
+                print(item['snippet']['title'])
+                fetched_video_id = item['id']['videoId']
+                found = True
+                try:
+                    m = main_db.objects.get(video_id=fetched_video_id)
+                except ObjectDoesNotExist:
+                    found = False
 
-            if found:
-                continue
-            else:
-                v_title = item['snippet']['title']
-                v_description = item['snippet']['description']
-                v_thumb_url = item['snippet']['thumbnails']['default']['url']
-                v_published_date = item['snippet']['publishedAt'][:10]
+                if found:
+                    continue
+                else:
+                    v_title = item['snippet']['title']
+                    v_description = item['snippet']['description']
+                    v_thumb_url = item['snippet']['thumbnails']['default']['url']
+                    v_published_date = item['snippet']['publishedAt'][:10]
 
-                query = main_db(video_id=fetched_video_id, video_title=v_title,
-                                description=v_description, published_date=v_published_date,
-                                thumbnail_url=v_thumb_url)
-                query.save()
+                    query = main_db(video_id=fetched_video_id, video_title=v_title,
+                                    description=v_description, published_date=v_published_date,
+                                    thumbnail_url=v_thumb_url)
+                    query.save()
+        except:
+            print('ACTUAL STREAM AFTER THIS--------------------')
+            list_index = list_index + 1
+            list_index = list_index % 5
+            # print(list_index)
+            api_key = api_key_list[list_index]
+            # print(api_key)
+
         time.sleep(5)
 
 
-t = threading.Thread(target=fetch_from_youtube, args=(keyword, api_key,))
+t = threading.Thread(target=fetch_from_youtube,
+                     args=(keyword, api_key_list[0],))
 t.daemon = True
-# t.start()
-
-# while True:
-#     sameer()
-#     time.sleep(5)
+t.start()
 
 
 def index(request):
