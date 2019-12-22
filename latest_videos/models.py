64861bdc3c14f161fab977b0e88c8df68fbc5237
Sameer Choubey <sameer.choubey98@gmail.com>
2019-12-22 11:28:21 +0530

    Work done till now. a) Slight modification in DB Models as per requirement. b) Created the function to fetch data from YouTube API and stored it. c) Added threading for running the function as a child process (which will run every n second).

--- latest_videos/models.py
+++ latest_videos/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
 
 class main_db(models.Model):
-    index = models.IntegerField(primary_key=True, unique=True)
+    id = models.IntegerField(primary_key=True, unique=True)
+    video_id = models.CharField(max_length=200)
     video_title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     published_date = models.DateField()
     thumbnail_url = models.CharField(max_length=200)
-    video_url = models.CharField(max_length=200)
 
     def __str__(self):
         return self.video_title
--- latest_videos/views.py
+++ latest_videos/views.py
@@ -1,7 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apiclient.discovery import build
+from django.core.exceptions import ObjectDoesNotExist
+from .models import main_db
+import time
+import threading
+import random
+
+api_key = 'AIzaSyCJtkbrA7_U3SjJYHEAU5Ha0tZzOdsSqy8'
+keyword = 'Premier League'
+
+def fetch_from_youtube(keyword, api_key):
+    while True:
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        req = youtube.search().list(q=keyword, part='snippet', type='video', maxResults=50)
+        res = req.execute()
+
+        for item in res['items']:
+            print(item['snippet']['title'])
+            fetched_video_id = item['id']['videoId']
+            found = True
+            try:
+                m = main_db.objects.get(video_id=fetched_video_id)
+            except ObjectDoesNotExist:
+                found = False
+
+            if found:
+                continue
+            else:
+                v_title = item['snippet']['title']
+                v_description = item['snippet']['description']
+                v_thumb_url = item['snippet']['thumbnails']['default']['url']
+                v_published_date = item['snippet']['publishedAt'][:10]
+
+                query = main_db(video_id=fetched_video_id, video_title=v_title,
+                                description=v_description, published_date=v_published_date,
+                                thumbnail_url=v_thumb_url)
+                query.save()
+        time.sleep(5)
+
+
+t = threading.Thread(target=fetch_from_youtube, args=(keyword, api_key,))
+t.daemon = True
+# t.start()
+
+# while True:
+#     sameer()
+#     time.sleep(5)
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+    m = main_db.objects.get(id=random.randint(1, 50))
+    print(m.thumbnail_url)
+    m = {"thumbnail": str(m.thumbnail_url), "title": str(m.video_title)}
+    return render(request, 'index.html', {'m': m})
